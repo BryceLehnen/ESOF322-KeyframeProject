c6d7e99ce3d5f8008c3dafec4d2dcebb8b46a9a9
BryceLehnen <103777604+BryceLehnen@users.noreply.github.com>
2022-04-15 15:07:17 -0600

    Another test commit

---
+++ ESOF 322 - Keyframe Project/src/Duck.java
@@ -0,0 +1,29 @@
+
+public abstract class Duck {
+	QuackBehavior quackBehavior;
+	FlyBehavior flyBehavior;
+	
+	public Duck() {}
+	
+	public abstract void display();
+	
+	public void performQuack() {
+		quackBehavior.quack();
+	}
+	
+	public void performFly() {
+		flyBehavior.fly();
+	}
+	
+	public void swim() {
+		System.out.println("All ducks float, even decoys!");
+	}
+	
+	public void setFlyBehavior(FlyBehavior fb) {
+		flyBehavior = fb;
+	}
+	
+	public void setQuackBehavior(QuackBehavior qb) {
+		quackBehavior = qb;
+	}
+}
