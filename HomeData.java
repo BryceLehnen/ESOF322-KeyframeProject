dcfc9de66a6fbfc6de8e62c9c4b7d3e027c69499
20njohnson <103784178+20njohnson@users.noreply.github.com>
2022-04-15 16:19:04 -0600

    Update HomeData.java

--- HomeData.java
+++ HomeData.java
@@ -1,4 +1,38 @@
 
 public class HomeData {
-
+  void registerObserver(){
+    
+  }
+  
+  void removeObserver(){
+    
+  }
+  
+  void notifyObservers(){
+    
+  }
+  
+  void getTime(){
+    
+  }
+  
+  void getVideo(){
+    
+  }
+  
+  void getAudio(){
+    
+  }
+  
+  void getAdditional(){
+    
+  }
+  
+  void getShareInformation(){
+    
+  }
+  
+  void getDeviceType(){
+    
+  }
 }
