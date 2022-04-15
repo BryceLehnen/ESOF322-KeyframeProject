7cbb626666e8e69ffdf48845681caed8840a152c
LukasB4 <103784087+LukasB4@users.noreply.github.com>
2022-04-15 17:24:01 -0600

    Update KeyframeVideoDisplay.java

--- KeyframeVideoDisplay.java
+++ KeyframeVideoDisplay.java
@@ -1,5 +1,7 @@
+//Lukas Bernard - Framework, Implementation and override
 
-public class KeyframeVideoDisplay implements observer, PreviewData{
+public class KeyframeVideoDisplay implements observer, PreviewData
+{
   @Override
   void update(/* some parameter */)
   {
