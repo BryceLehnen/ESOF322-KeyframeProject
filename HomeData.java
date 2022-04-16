686ca4934e4629016d1b9b944d168ad540f9b565
LukasB4 <103784087+LukasB4@users.noreply.github.com>
2022-04-15 20:19:51 -0600

    Revamped Constructor, added variables, filled methods

--- HomeData.java
+++ HomeData.java
@@ -1,71 +1,89 @@
 //Lukas Bernard constructor
+import java.util.List;
+import java.util.ArrayList;
 
 public class HomeData implements subject
 {
 
-  HomeData(String t, boolean v, boolean a, String additionalInformation, String device)
+  private List<observer> displayTypes = new ArrayList<>();
+  private KeyframeVideoDisplay video;
+  private KeyframeTimeDisplay time;
+  private KeyframeAudioDisplay audio;
+  private KeyframeAdditionalDisplay additional;
+  private KeyframeShareDisplay share;
+  private KeyframeDeviceDisplay deviceType;
+
+  HomeData()
   { 
-    KeyframeVideoDisplay video = new KeyframeVideoDisplay();
-    KeyframeTimeDisplay time = new KeyframeTimeDisplay();
-    KeyframeAudioDisplay audio = new KeyframeAudioDisplay();
-    KeyframeAdditionalDisplay additional = new KeyframeAdditionalDisplay();
-    KeyframeDeviceDisplay deviceType = new KeyframeDeviceDisplay();
+    video = new KeyframeVideoDisplay();
+    time = new KeyframeTimeDisplay();
+    audio = new KeyframeAudioDisplay();
+    additional = new KeyframeAdditionalDisplay();
+    share = new KeyframeShareDisplay();
+    deviceType = new KeyframeDeviceDisplay();
     
     registerObserver(video);
     registerObserver(time);
     registerObserver(audio);
     registerObserver(additional);
     registerObserver(deviceType);
-
   }
 
   void registerObserver(observer n)
   {
-    
+    displayTypes.add(n);
   }
   
   void removeObserver(observer n)
   {
-    
+    displayTypes.remove(n);
   }
   
-  void notifyObservers(String M)
+  void notifyObservers(observer n, string information)
   {
+    n.update(information);
+  }
 
-  }
-
-  void notifyObservers(boolean state)
+  void notifyObservers(observer n, boolean state)
   {
-
+    n.update(state);
   }
   
-  void getTime()
+  void getTime(String timeState)
   {
-    
+    notifyObservers(time, timeState);
   }
   
-  void getVideo()
+  void getVideo(boolean videoState)
   {
-    
+    notifyObservers(video, videoState);
   }
   
-  void getAudio()
+  void getAudio(boolean audioState)
   {
-    
+    notifyObservers(audio, audioState);
   }
   
-  void getAdditional()
+  void getAdditional(String additionalInformation)
   {
-    
+    notifyObservers(additional, additionalInformation);
   }
   
-  void getShareInformation()
+  void getShareInformation(boolean shareInformation)
   {
-    
+    notifyObservers(share, shareInformation);
   }
   
-  void getDeviceType()
+  void getDeviceType(String device)
   {
-    
+    notifyObservers(deviceType, device);
+  }
+  
+  void removeObserverSet()
+  {
+    for(int i = 0; i < displayTypes.size(); i++)
+    {
+      removeObserver(displayTypes[i]);
+    }
   }
 }
