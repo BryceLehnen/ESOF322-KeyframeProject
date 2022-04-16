f9b8afcf319734666c598107240f602a7a6b743b
20njohnson <103784178+20njohnson@users.noreply.github.com>
2022-04-15 19:06:58 -0600

    updated update and display methods

--- KeyframeAdditionalDisplay.java
+++ KeyframeAdditionalDisplay.java
@@ -1,19 +1,28 @@
 //Lukas Bernard - Framework, Implementation and override
 //Nathan Johnson - Method framework, parameter input
 
+import java.lang.String;
+
 public class KeyframeAdditionalDisplay implements observer, PreviewData
 {
 
+  private boolean dataStored = false;
   @Override
-  public void update(/* some parameter */)
+  public void update(boolean test)
   {
-    
+      dataStored = test;
   }
   
   @Override
   public void display()
   {
-    
+    if (dataStored)
+    {
+      System.out.println("There is an additional element"); // change this to what it is
+    }
+    else if (!dataStored){
+      System.out.pirntln("THere is no additional occurrences to display");
+    }
   }
 
 }
