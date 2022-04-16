12c577ed7845e9375d1f049ded82a10c86e6de47
Colby <robertsoc@hotmail.com>
2022-04-15 20:33:58 -0600

    Finished up driver

--- Driver.java
+++ Driver.java
@@ -6,6 +6,7 @@ public class Driver {
 	public static void main(String[] args){
 		boolean done = false;
 		while(!done){
+			HomeData hd = new HomeData();
 			Scanner in = new Scanner(System.in);
 			System.out.println("Enter one of the following system:\n\tq - quit\n\tkf - keyframe\n");
 			String choice = in.nextLine();
@@ -16,8 +17,16 @@ public class Driver {
 				try(BufferedReader reader = new BufferedReader(new FileReader(day))){
 					String line = reader.readLine();
 					while(line!=null){
+						String[] l = line.split(", ");
+						if(Boolean.parseBoolean(l[1]) || Boolean.parseBoolean(l[2])){
+							hd.getTime(l[0]);
+							hd.getVideo(Boolean.parseBoolean(l[1]));
+							hd.getAudio(Boolean.parseBoolean(l[2]));
+							hd.getAdditional(l[3]);
+							hd.getDeviceType(l[4]);
+							hd.getShareInformation(Boolean.parseBoolean(l[5]));
+						}
 						line = reader.readLine();
-						
 					}
 				}	
 				catch(IOException e){
