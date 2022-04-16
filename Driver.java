24063a011a395b7960764e659037672b010b6e01
Colby <robertsoc@hotmail.com>
2022-04-15 19:22:32 -0600

    Changing driver

--- Driver.java
+++ Driver.java
@@ -1,10 +1,34 @@
-
-public class Driver 
-{
+import java.io.*;
+import java.util.Scanner;
+//Colby Roberts - Made driver
+public class Driver {
 	
-	public static void main(String[] args) 
-	{
-		int n;
-		int poo;
+	public static void main(String[] args){
+		boolean done = false;
+		while(!done){
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter one of the following system:\n\tq - quit\n\tkf - keyframe\n");
+			String choice = in.nextLine();
+			if(choice.equals("kf")){
+				System.out.println("Enter day to display keyframes of (format mmddyyy w/0's):\n");
+				String day = in.nextLine() + ".txt";
+				in.close();
+				try(BufferedReader reader = new BufferedReader(new FileReader(day))){
+					String line = reader.readLine();
+					while(line!=null){
+						line = reader.readLine();
+						
+					}
+				}	
+				catch(IOException e){
+					System.out.println(e);
+				}
+			}
+			else{
+				done = true;
+				in.close();
+			}
+		}
 	}
+
 }
--- HomeData.java
+++ HomeData.java
@@ -11,7 +11,7 @@ public class HomeData implements subject
     
   }
   
-  void notifyObservers(//stringhere)
+  void notifyObservers(/*stringhere*/)
   {
     
   }
