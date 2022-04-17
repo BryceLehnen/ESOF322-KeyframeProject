fe374735be20f7d014257b7803cc898d10ec9927
BryceLehnen <dlkl46.bl@gmail.com>
2022-04-16 19:05:42 -0600

    KeyDevice update() done

--- KeyframeDeviceDisplay.java
+++ KeyframeDeviceDisplay.java
@@ -1,17 +1,29 @@
 //Lukas Bernard - Framework, Implementation and override
 //Nathan Johnson - Method framework, parameter input
+// Bryce Lehnen - update method
 
 public class KeyframeDeviceDisplay implements observer, PreviewData
 {
 
+  String deviceType;    // Holds the type of device (ie. doorlock, camera, etc)
+
   @Override
-  void update(/* some parameter */)
+  public <T extends Comparable<T>> void update(T information)
   {
-    
+    // Checking to ensure important* info was sent
+    // Info was sent
+    if (!information.equals(null)) {
+      deviceType = (String) information;
+      display();
+    }
+    // No important info (REMOVE AFTER TESTING)
+    else {
+      System.out.println("NO DEVICE INFO");
+    }
   }
   
   @Override
-  void display()
+  public void display()
   {
     
   }
