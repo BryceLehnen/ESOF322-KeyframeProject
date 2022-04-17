4ab91d40c0b817759ec73c44bd2fe6548100a90d
BryceLehnen <dlkl46.bl@gmail.com>
2022-04-16 19:08:05 -0600

    KeyAudio update() done

--- KeyframeAudioDisplay.java
+++ KeyframeAudioDisplay.java
@@ -4,13 +4,21 @@
 public class KeyframeAudioDisplay implements PreviewData, observer
 {
     @Override
-    void update(/* some parameter */)
+    public <T extends Comparable<T>> void update(T state)
     {
-        
+        // Checking state (boolean) if it is true or false
+        // True, then update display
+        if (state.equals(true)) {
+            display();
+        }
+        // False, then skip (REMOVE AFTER TESTING)
+        else if (state.equals(false)) {
+            System.out.println("AUDIO IS REGISTERED AS FALSE");
+        }
     }
     
     @Override
-    void display()
+    public void display()
     {
         
     }
