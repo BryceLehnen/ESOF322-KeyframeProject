a80de045fe04177fa01875e3172fe9defbc6cdd4
BryceLehnen <dlkl46.bl@gmail.com>
2022-04-18 09:55:36 -0600

    changed datastored variable to dataStored

--- KeyframeAdditionalDisplay.java
+++ KeyframeAdditionalDisplay.java
@@ -4,14 +4,14 @@
 
 public class KeyframeAdditionalDisplay implements observer, PreviewData {
 
-  private String datastored;  // Contains important info about incident
+  private String dataStored;  // Contains important info about incident
   
   @Override
   public <T extends Comparable<T>> void update(T information) {
       // Checking if important* info was sent
       // was sent
       if (!information.equals("nothing found")) {
-        datastored = (String) information;
+        dataStored = (String) information;
         display();
       }
       // No important info (REMOVE AFTER TESTING)
