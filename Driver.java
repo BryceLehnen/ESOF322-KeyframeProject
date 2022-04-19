107a69734c46a3dd6d437ba7e5a5f4dcf4dfff06
20njohnson <103784178+20njohnson@users.noreply.github.com>
2022-04-18 20:27:34 -0600

    Update Driver Method for proper function

--- Driver.java
+++ Driver.java
@@ -1,22 +1,26 @@
+package keyframe;
+
+// Colby Roberts - Made driver
+// Nathan Johnson - Edited driver to secure proper function and ideal formatting
+
 import java.io.*;
 import java.util.Scanner;
-//Colby Roberts - Made driver
+
 public class Driver {
 	
 	public static void main(String[] args) {
 		boolean done = false;
 		while(!done) {
-			HomeData hd = new HomeData();
+            HomeData hd = new HomeData();
 			Scanner in = new Scanner(System.in);
 			System.out.println("Enter one of the following system:\n\tq - quit\n\tkf - keyframe\n");
 			String choice = in.nextLine();
 			if(choice.equals("kf")) {
-				System.out.println("Enter day to display keyframes of (format mmddyyy w/0's):\n");
+				System.out.println("Enter day to display keyframes of (format mmddyyyy w/0's):\n");
 				String day = in.nextLine() + ".txt";
-				in.close();
 				try(BufferedReader reader = new BufferedReader(new FileReader(day))) {
 					String line = reader.readLine();
-					while(line!=null){
+					while(line != null) {
 						String[] l = line.split(", ");
 						if(Boolean.parseBoolean(l[1]) || Boolean.parseBoolean(l[2])) {
 							hd.getTime(l[0]);
@@ -25,6 +29,7 @@ public class Driver {
 							hd.getAdditional(l[3]);
 							hd.getDeviceType(l[4]);
 							hd.getShareInformation(Boolean.parseBoolean(l[5]));
+                            System.out.println("");
 						}
 						line = reader.readLine();
 					}
@@ -34,10 +39,9 @@ public class Driver {
 				}
 			}
 			else {
+                in.close();
 				done = true;
-				in.close();
 			}
 		}
 	}
-
 }
